f0d2abaf9551be4a686238b1ae9bfc8ed5e29fa6
hollisd09 <hollisd09@gmail.com>
2016-02-18 15:07:29 -0600

    Slight refactoring controllers/note.js

--- routes/note.js
+++ routes/note.js
@@ -3,8 +3,11 @@ const router = express.Router();
 
 const note = require('../controllers/note');
 
+router.get('/notes', note.index);
 router.get('/notes/new', note.newNote);
 router.get('/notes/:id', note.show);
+router.get('/notes/:id/edit', note.edit);
+router.put('/notes/:id/update', note.update);
 router.get('/notes/:id', note.destroy);
 router.post('/notes', note.create);
 
