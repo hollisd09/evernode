3ccb569982c075db7b8b2babaf59e734a27b7557
hollisd09 <hollisd09@gmail.com>
2016-02-18 11:38:00 -0600

    Adding all the files

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 'use strict';
 
+const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
+
+const note = require('./routes/note');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(note);
+
 app.get('/', (req, res) => {
-  res.send('Server Running just as fast as it can');
+  res.send('Server Running');
 });
 
-app.listen(port, () => {
-  console.log(`Evernode server running on port: ${port}`);
+mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
+  if (err) throw err;
+
+  app.listen(port, () => {
+    console.log(`Evernode server running on port: ${port}`);
+  });
 });
